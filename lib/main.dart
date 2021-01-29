34ccaad61c5c450d0e66515f49f52bd9d4b6ca21
Oorjit Chowdhary <oorjit.chowdhary@hotmail.com>
2021-01-30 03:26:03 +0530

    OCR + validation (#4)
    * camera intent with image bytes
    
    * ocr + validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'screens/home.dart';
 import 'screens/login.dart';
 import 'screens/register.dart';
@@ -9,34 +10,44 @@ import 'screens/verification/1.dart';
 import 'screens/verification/2.dart';
 import 'screens/verification/3.dart';
 
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  final cameras = await availableCameras();
-  final firstCamera = cameras.first;
-  runApp(Kyt(firstCamera: firstCamera));
+  runApp(Kyt());
 }
 
 class Kyt extends StatelessWidget {
-  final CameraDescription firstCamera;
-
-  const Kyt({Key key, this.firstCamera}) : super(key: key);
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'KYT',
-      theme: ThemeData(fontFamily: "lexenddeca"),
-      initialRoute: Welcome.id,
-      routes: {
-        Home.id: (context) => Home(),
-        Login.id: (context) => Login(),
-        Register.id: (context) => Register(),
-        Welcome.id: (context) => Welcome(),
-        Upload.id: (context) => Upload(camera: firstCamera),
-        Verification1.id: (context) => Verification1(),
-        Verification2.id: (context) => Verification2(),
-        Verification3.id: (context) => Verification3(),
-      },
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          // return error widget
+          return Text('');
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'KYT',
+            theme: ThemeData(fontFamily: "lexenddeca"),
+            initialRoute: Welcome.id,
+            routes: {
+              Login.id: (context) => Login(),
+              Register.id: (context) => Register(),
+              Welcome.id: (context) => Welcome(),
+              Upload.id: (context) => Upload(),
+              Verification1.id: (context) => Verification1(),
+              Verification2.id: (context) => Verification2(),
+              Verification3.id: (context) => Verification3(),
+            },
+          );
+        }
+
+        // show loader while initializing
+        return CircularProgressIndicator();
+      }
     );
   }
 }
--- lib/screens/upload.dart
+++ lib/screens/upload.dart
@@ -1,114 +1,169 @@
 import 'dart:async';
-import 'dart:io';
+import 'dart:convert';
 import 'dart:ui';
 
-import 'package:camera/camera.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:kyt/global/myColors.dart';
 import 'package:kyt/global/mySpaces.dart';
-import 'package:kyt/global/myStrings.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart' show join;
+import 'package:http/http.dart' as http;
 
 class Upload extends StatefulWidget {
   static String id = "upload";
-  final CameraDescription camera;
-
-  const Upload({ Key key, @required this.camera }) : super(key: key);
 
   @override
   _UploadState createState() => _UploadState();
 }
 
 class _UploadState extends State<Upload> {
-  CameraController _controller;
-  Future<void> _initializeControllerFuture;
-  String imagePath;
-  bool noImage = true;
+  final _auth = FirebaseAuth.instance;
+  String imagePath, testName;
+  bool validTest;
 
-  @override
-  void initState() {
-    super.initState();
-    _controller = CameraController(widget.camera, ResolutionPreset.medium);
-    _initializeControllerFuture = _controller.initialize();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: MyColors.darkPrimary,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          Center(
-            child: Text(
-              MyStrings.travellerLabel,
-              style: Theme.of(context).textTheme.headline6.copyWith(color: MyColors.lightGrey),
-            ),
-          ),
-          MySpaces.vSmallGapInBetween,
-          noImage ? FutureBuilder<void>(
-            future: _initializeControllerFuture,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                return AspectRatio(
-                    aspectRatio: _controller.value.aspectRatio,
-                    child: CameraPreview(_controller));
-              } else {
-                return Center(child: CircularProgressIndicator());
-              }
-            },
-          ) : SizedBox(),
-          !noImage ? ShowImage(imagePath: imagePath) : SizedBox()
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.camera_alt),
-        onPressed: () async {
-          try {
-            await _initializeControllerFuture;
-            final path = join((await getTemporaryDirectory()).path, '${DateTime.now()}.png');
-            await _controller.takePicture(path);
-
-            setState(() {
-              imagePath = path;
-              noImage = false;
-            });
-
-          } catch (e) {
-            print(e);
-          }
+  Future<String> getTextFromOCR(List<int> imageBytes) async {
+    final ocrEndpoint = Uri.parse('https://kyt-app-ocr.cognitiveservices.azure.com/vision/v3.1/ocr');
+    final azureResponse = await http.post(
+        ocrEndpoint,
+        headers: <String, String>{
+          'Ocp-Apim-Subscription-Key': 'b583a5bb13ff448399982684386e48ac',
+          'Content-Type': 'application/octet-stream'
         },
-      ),
+        body: imageBytes
     );
-  }
-}
 
-class ShowImage extends StatelessWidget {
-  final String imagePath;
-  const ShowImage({ Key key, this.imagePath }) : super(key: key);
+    String ocrText = '';
+
+    if (azureResponse.statusCode == 200) {
+      Map<String, dynamic> responseMap = json.decode(azureResponse.body);
+
+      for (var region in responseMap['regions']) {
+        for (var lines in region['lines']) {
+          for (var words in lines['words']) {
+            ocrText += words['text'] + ' ';
+          }
+        }
+      }
+    }
+
+    return ocrText;
+  }
+
+  Future<bool> validateText(String text, String email, String testName) async {
+    final String validationEndpoint = 'https://kyt-api.azurewebsites.net/verify';
+    final validationResponse = await http.post(
+        validationEndpoint,
+        body: <String, String>{
+          'email': email,
+          'testName': testName,
+          'timestamp': '${DateTime.now()}',
+          'text': text
+        }
+    );
+
+    if (validationResponse.statusCode == 200) {
+      bool valid = json.decode(validationResponse.body)['boolean'];
+      return valid;
+    }
+
+    print('nothing happened thus returning false');
+    Scaffold.of(context).showSnackBar(SnackBar(content: Text('An unknown error occurred. We were unable to verify the test.')));
+    return false;
+  }
 
   @override
   Widget build(BuildContext context) {
-    print(imagePath);
+    final user = _auth.currentUser;
 
-    File image = File(imagePath);
-    List<int> imageBytes = image.readAsBytesSync();
-    print(imageBytes);
+    return Scaffold(
+      backgroundColor: MyColors.offWhite,
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 30),
+        child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Verify your tests',
+                  style: Theme.of(context).textTheme.headline5.copyWith(
+                  fontWeight: FontWeight.w600, color: MyColors.darkPrimary)
+                ),
+                MySpaces.vMediumGapInBetween,
+                TextField(
+                  onChanged: (value) {
+                    testName = value;
+                  },
+                  decoration: InputDecoration(
+                    counter: Container(),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                    ),
+                    hintStyle: Theme.of(context)
+                        .textTheme
+                        .headline6
+                        .copyWith(color: Colors.grey[800]),
+                    hintText: 'Test name',
+                    fillColor: Colors.white54,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: MyColors.darkGrey, width: 2.0),
+                    ),
+                  ),
+                )
+              ],
+            ),
+        ),
+      ),
+      floatingActionButton: Builder(
+        builder: (context) => FloatingActionButton(
+          child: Icon(Icons.camera_alt),
+          onPressed: () async {
+            showDialog(
+              context: context,
+              barrierDismissible: true,
+              child: AlertDialog(
+               content: SingleChildScrollView(
+                 child: ListBody(
+                   children: <Widget>[
+                     GestureDetector(
+                       child: Text('Take a picture'),
+                       onTap: () async {
+                         Navigator.of(context, rootNavigator: true).pop(); // clears dialog box
+                         final imageFromCamera = await ImagePicker.pickImage(source: ImageSource.camera);
+                         final imageBytes = imageFromCamera.readAsBytesSync();
+                         String ocrText = await getTextFromOCR(imageBytes);
+                         validTest = await validateText(ocrText, user.email, testName);
+                         print('camera intent valid test result: $validTest');
 
-    // TODO: post to Azure CV OCR to get text
-    // post text to api
+                         !validTest
+                             ? Scaffold.of(context).showSnackBar(SnackBar(content: Text('We couldn\'t verify your test results. Please try again.')))
+                             : Scaffold.of(context).showSnackBar(SnackBar(content: Text('Valid test result uploaded. You\'re good to go!')));
+                       }
+                     ),
+                     Padding(padding: EdgeInsets.all(8.0)),
+                     GestureDetector(
+                       child: Text('Choose from gallery'),
+                       onTap: () async {
+                         Navigator.of(context, rootNavigator: true).pop(); // clears dialog box
+                         final imageFromGallery = await ImagePicker.pickImage(source: ImageSource.gallery);
+                         final imageBytes = imageFromGallery.readAsBytesSync();
+                         String ocrText = await getTextFromOCR(imageBytes);
+                         validTest = await validateText(ocrText, user.email, testName);
+                         print('gallery intent valid test result: $validTest');
 
-    return Container(
-      child: Image.file(File(imagePath))
+                         !validTest
+                             ? Scaffold.of(context).showSnackBar(SnackBar(content: Text('We couldn\'t verify your test results. Please try again.')))
+                             : Scaffold.of(context).showSnackBar(SnackBar(content: Text('Valid test result uploaded. You\'re good to go!')));
+                       },
+                     )
+                   ],
+                 )
+               )
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
