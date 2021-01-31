0e3e76142e73ad4d175cbc5c73d98de5af4ee22d
Oorjit Chowdhary <oorjit.chowdhary@hotmail.com>
2021-01-31 19:12:14 +0530

    home screen + get qr code (#5)

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,14 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:kyt/global/myColors.dart';
 import 'package:kyt/global/myDimens.dart';
 import 'package:kyt/global/mySpaces.dart';
-import 'package:kyt/global/myStrings.dart';
-import 'package:kyt/screens/verification/1.dart';
-import 'package:kyt/widgets/verificationCard.dart';
-import 'verification/2.dart';
-import 'verification/3.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:firebase_auth/firebase_auth.dart';
+
+Future<String> getUserTests(String email) async {
+  final testsEndpoint = Uri.parse('https://kyt-api.azurewebsites.net/profile?email=$email');
+  final response = await http.get(testsEndpoint);
+
+  if (response.statusCode == 200) {
+    return response.body;
+  }
+
+  return null;
+}
 
 class Home extends StatefulWidget {
   static String id = "home";
@@ -18,111 +24,93 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  void getCurrentUserId() async {
-    final auth = FirebaseAuth.instance;
-    final user = auth.currentUser;
-    final uid = user.uid;
-    print(uid);
-  }
-
-  List medicalRecords;
-  String covidVaccine;
-  String name;
-
-  @override
-  void initState() {
-    super.initState();
-    fetchJsonData();
-    getCurrentUserId();
-  }
-
-  void fetchJsonData() async {
-    var url = 'https://knowyourtraveller.herokuapp.com/api/users/123456789012';
-    // Await the http get response, then decode the json-formatted response.
-    var response = await http.get(url);
-    if (response.statusCode == 200) {
-      var jsonResponse = response.body;
-      var name = json.decode(jsonResponse)['name'];
-      var medicalRecords =
-          json.decode(jsonResponse)['medicalRecords']['covidVaccine'];
-    } else {
-      print('Request failed with status: ${response.statusCode}.');
-    }
-  }
+  final user = FirebaseAuth.instance.currentUser;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomPadding: true,
-      backgroundColor: MyColors.offWhite,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Column(
-            children: <Widget>[
-              Container(
-                color: MyColors.darkPrimary,
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height*0.3,
-                padding: EdgeInsets.all(MyDimens.double_50),
-                child: Container(
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                        image: NetworkImage('https://github.com/rchtgpt.png'),
-                    ),
-                  ),
-                ),
+    return FutureBuilder(
+      future: getUserTests(user.email),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(child: CircularProgressIndicator());
+        }
 
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: MyDimens.double_30, vertical: MyDimens.double_15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      MyStrings.sampleName + "'s Healthcare Records",
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline5
-                          .copyWith(
+        final userProfile = json.decode(snapshot.data);
+
+        return Scaffold(
+            backgroundColor: MyColors.offWhite,
+            body: Container(
+                child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
                           color: MyColors.darkPrimary,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    MySpaces.vLargeGapInBetween,
-                    Container(
-                      child: Column(
-                        children: [
-                          VerificationCard(
-                            iconBool: true,
-                            route: Verification1.id,
-                            text: MyStrings.test1Label,
+                          width: MediaQuery.of(context).size.width,
+                          height: MediaQuery.of(context).size.height*0.3,
+                          padding: EdgeInsets.all(MyDimens.double_10),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                image: NetworkImage('${userProfile['pfp']}'),
+                              ),
+                            ),
                           ),
-                          MySpaces.vSmallGapInBetween,
-                          VerificationCard(
-                            iconBool: false,
-                            route: Verification2.id,
-                            text: MyStrings.test2Label,
-                          ),
-                          MySpaces.vSmallGapInBetween,
-                          VerificationCard(
-                            iconBool: false,
-                            route: Verification3.id,
-                            text: MyStrings.test3Label,
-                          ),
-                          MySpaces.vSmallGapInBetween,
-                        ],
-                      ),
+
+                        ),
+                        Container(
+                          child: Container(
+                            margin: EdgeInsets.symmetric(
+                                horizontal: MyDimens.double_30,
+                                vertical: MyDimens.double_15
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  '${userProfile['name']}\'s\nHealth-care records',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headline5
+                                      .copyWith(
+                                      color: MyColors.darkPrimary,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                MySpaces.vMediumGapInBetween,
+                                Container(
+                                  height: 250.0,
+                                  child: ListView.builder(
+                                    shrinkWrap: true,
+                                    itemCount: userProfile['tests'].length,
+                                    itemBuilder: (context, index) {
+                                      final test = userProfile['tests'][index];
+                                      return ListTile(
+                                          trailing: test['status'] == 'valid'
+                                              ? Icon(Icons.check, color: MyColors.green)
+                                              : Icon(Icons.clear, color: Colors.red),
+                                          title: Text(
+                                              test['testName'],
+                                              style: TextStyle(
+                                                  color: Colors.black,
+                                                  fontSize: 18.0
+                                              )
+                                          )
+                                      );
+                                    },
+                                  ),
+                                ),
+                              ]
+                            ),
+                          )
+                        ),
+                      ],
                     )
-                  ],
-                ),
-              )
-            ],
-          )
-        ],
-      ),
+                )
+            ),
+        );
+      }
     );
   }
 }
